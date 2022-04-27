cef6c0d1ad017d92e69f59eff585d184d48c445e
batesasho <agenov@gmail.com>
2022-04-27 18:52:39 +0300

    bug fixed in celery tasks.py

--- nutrition_blog/nutrition_blog/email_client/tasks.py
+++ nutrition_blog/nutrition_blog/email_client/tasks.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 @shared_task
 def consultation_contact_message_sending(*args):
+
     subject, from_email, message, to_email = args
     try:
         send_mail(subject = subject, message = message, from_email = from_email, recipient_list = to_email)
